3264eb5e323a56f3c11e73165e96df43f38997f8
hussaintaha <hussaintaha500@gmail.com>
2024-10-25 13:11:40 +0530

    check updated css

--- solar-combo-react/src/App.jsx
+++ solar-combo-react/src/App.jsx
@@ -5,7 +5,7 @@ import Modal from './component/Modal';
 
 const App = () => {
 
-  console.log(" ========== 2222222 =========");
+  console.log(" ========== 3333333333333333 =========");
 
   const [loading, setLoading] = useState(false);
   const [activecartButton, setActiveCartButton] = useState(true)
