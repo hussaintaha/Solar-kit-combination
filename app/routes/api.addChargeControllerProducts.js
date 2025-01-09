38898eeaab56aeda08878b40b590a81dfb6b652b
hussaintaha <hussaintaha500@gmail.com>
2025-01-09 13:05:38 +0530

    updated solar pannel table

--- app/routes/api.addChargeControllerProducts.js
+++ app/routes/api.addChargeControllerProducts.js
@@ -9,10 +9,11 @@ export const action = async ({ request }) => {
     const { session } = await authenticate.admin(request);
     const { selectHarvestValue, selected } = selectedData;
 
-    const updatedProducts = await Promise.all(selected.map(async (product) => {
-      const splitProductId = product.product.id.split("/")[4];
+    const updatedProducts = await Promise.all(
+      selected.map(async (product) => {
+        const splitProductId = product.product.id.split("/")[4];
 
-      const query = `
+        const query = `
         query GetProduct {
           product(id: "gid://shopify/Product/${splitProductId}") {
             id
@@ -21,34 +22,39 @@ export const action = async ({ request }) => {
         }
       `;
 
-      const response = await shopifyGraphql(session, apiVersion, query);
-      if (response && response.data && response.data.product) {
-        return {
-          ...product,
-          handle: response.data.product.handle
-        };
-      }
+        const response = await shopifyGraphql(session, apiVersion, query);
+        console.log("response 2222 === ", response);
 
-    }));
-
-    const updatedAirConditionerEntry = await chargeControllerCollection.findOneAndUpdate(
-      { harvestValue: selectHarvestValue },
-      { $addToSet: { products: { $each: updatedProducts } } }, // Change to $push if needed
-      { new: true }
+        if (response && response.data && response.data.product) {
+          return {
+            ...product,
+            handle: response.data.product.handle,
+          };
+        }
+      }),
     );
 
+    const updatedAirConditionerEntry =
+      await chargeControllerCollection.findOneAndUpdate(
+        { harvestValue: selectHarvestValue },
+        { $addToSet: { products: { $each: updatedProducts } } }, // Change to $push if needed
+        { new: true },
+      );
+
     if (!updatedAirConditionerEntry) {
-      console.log("No matching entry found for harvestValue:", selectHarvestValue);
+      console.log(
+        "No matching entry found for harvestValue:",
+        selectHarvestValue,
+      );
       return json({ success: false, message: "No matching entry found." });
     }
 
-    const updatedEntry = await chargeControllerCollection.findOne({ harvestValue: selectHarvestValue });
+    const updatedEntry = await chargeControllerCollection.findOne({
+      harvestValue: selectHarvestValue,
+    });
     return json({ success: true, updatedEntry });
-
   } catch (error) {
     console.log("error ========= ", error);
     return json({ success: false, message: error.message });
   }
 };
-
-
--- app/routes/api.addsolarPanelProducts.js
+++ app/routes/api.addsolarPanelProducts.js
@@ -1,6 +1,7 @@
 import { json } from "@remix-run/node";
 import solarPanelCollection from "../Database/collections/solarPanelModel";
 import { authenticate, apiVersion } from "../shopify.server";
+import { shopifyGraphql } from "./utils";
 
 export const action = async ({ request }) => {
   try {
@@ -8,10 +9,13 @@ export const action = async ({ request }) => {
     const { session } = await authenticate.admin(request);
     const { selectHarvestValue, selected } = selectedData;
 
-    const updatedProducts = await Promise.all(selected.map(async (product) => {
-      const splitProductId = product.product.id.split("/")[4];
+    console.log("selectHarvestValue ====== ", selectHarvestValue);
 
-      const query = `
+    const updatedProducts = await Promise.all(
+      selected.map(async (product) => {
+        const splitProductId = product.product.id.split("/")[4];
+
+        const query = `
         query GetProduct {
           product(id: "gid://shopify/Product/${splitProductId}") {
             id
@@ -20,29 +24,42 @@ export const action = async ({ request }) => {
         }
       `;
 
-      const response = await shopifyGraphql(session, apiVersion, query);
-      if (response && response.data && response.data.product) {
-        return {
-          ...product,
-          handle: response.data.product.handle
-        };
-      }
-    }));
+        const response = await shopifyGraphql(session, apiVersion, query);
+        console.log("response ======= ", response);
 
-    const updatedAirConditionerEntry = await solarPanelCollection.findOneAndUpdate(
-      { harvestValue: selectHarvestValue },
-      { $addToSet: { products: { $each: updatedProducts } } },
-      { new: true }
+        if (response && response.data && response.data.product) {
+          return {
+            ...product,
+            handle: response.data.product.handle,
+          };
+        }
+      }),
+    );
+
+    const updatedAirConditionerEntry =
+      await solarPanelCollection.findOneAndUpdate(
+        { harvestValue: selectHarvestValue },
+        { $addToSet: { products: { $each: updatedProducts } } },
+        { new: true },
+      );
+
+    console.log(
+      "updatedAirConditionerEntry ===== ",
+      updatedAirConditionerEntry,
     );
 
     if (!updatedAirConditionerEntry) {
-      console.log("No matching entry found for harvestValue:", selectHarvestValue);
+      console.log(
+        "No matching entry found for harvestValue:",
+        selectHarvestValue,
+      );
       return json({ success: false, message: "No matching entry found." });
     }
 
-    const updatedEntry = await solarPanelCollection.findOne({ harvestValue: selectHarvestValue });
+    const updatedEntry = await solarPanelCollection.findOne({
+      harvestValue: selectHarvestValue,
+    });
     return json({ success: true, updatedEntry });
-
   } catch (error) {
     console.log("error ========= ", error);
     return json({ success: false, message: error.message });
